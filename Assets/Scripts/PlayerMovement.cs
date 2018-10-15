efae0f04db0cef44db841e50312e51b2abde5740
Computer321 <stephen.luu@sbcglobal.net>
2018-10-15 16:34:56 -0700

    Made player movement feel more weighty. Added skybox for darkness.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,51 +4,58 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public KeyCode forward, backward, left, right, jump;
+    public KeyCode up, down, left, right, jump;
     private float horizontalMovement, verticalMovement;
     private Rigidbody rb;
     public float speed;
+    public float maxSpeed;
 
 	// Use this for initialization
 	void Start () {
         //TODO: Add some way to allow player to modify controls
         /*
-        forward = KeyCode.W;
-        backward = KeyCode.S;
+        up = KeyCode.W;
+        down = KeyCode.S;
         left = KeyCode.A;
         right = KeyCode.D;
         jump = KeyCode.Space;
         horizontalMovement = 0;
         verticalMovement = 0;
+        turnSpeed = .25f;
         speed = .10f;*/
+        horizontalMovement = 0;
+        verticalMovement = 0;
         rb = GetComponent<Rigidbody>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        horizontalMovement = 0;
-        verticalMovement = 0;
         float x = transform.position.x;
         float y = transform.position.y;
         float z = transform.position.z;
-        if (Input.GetKey(forward))
+
+        if (Input.GetKey(up))                   //Accelerate to max speed
         {
-            verticalMovement++;
+            if(verticalMovement < maxSpeed)
+                verticalMovement+= speed;
         }
 
-        if (Input.GetKey(backward))
+        if (Input.GetKey(down))
         {
-            verticalMovement--;
+            if (verticalMovement > -maxSpeed)
+                verticalMovement-= speed;
         }
 
         if (Input.GetKey(left))
         {
-            horizontalMovement--;
+            if (horizontalMovement > -maxSpeed)
+                horizontalMovement-= speed;
         }
 
         if (Input.GetKey(right))
         {
-            horizontalMovement++;
+            if (horizontalMovement < maxSpeed)
+                horizontalMovement +=speed;
         }
 
         if (Input.GetKey(jump))                         //Layer transition
@@ -56,6 +63,43 @@ public class PlayerMovement : MonoBehaviour {
 
         }
 
-        transform.Translate(horizontalMovement * speed, 0,verticalMovement * speed);
+        if(!Input.GetKey(left) && !Input.GetKey(right)) //Slow to standstill if not hitting any buttons
+        {
+            if (horizontalMovement < 0)
+            {
+                horizontalMovement += speed / 2;
+
+                if (horizontalMovement > 0)
+                    horizontalMovement = 0;     
+            }
+            else
+            {
+                horizontalMovement -= speed / 2;
+
+                if (horizontalMovement <= 0)
+                    horizontalMovement = 0;
+            }
+            
+        }
+
+        if (!Input.GetKey(up) && !Input.GetKey(down)) //Slow to standstill if not hitting any buttons
+        {
+            if (verticalMovement < 0)
+            {
+                verticalMovement += speed / 2;
+
+                if (verticalMovement > 0)
+                    verticalMovement = 0;
+            }
+            else
+            {
+                verticalMovement -= speed / 2;
+
+                if (verticalMovement <= 0)
+                    verticalMovement = 0;
+            }
+        }
+
+        transform.Translate(horizontalMovement, 0,verticalMovement);
     }
 }
