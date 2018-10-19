5f4c2111812e6a8338492f57213bc69e9c3fcd7a
JoshGutman <jng86@nau.edu>
2018-10-18 21:16:20 -0700

    Add Healthbar HUD

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.TakeDamage(1);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+    }
+}
