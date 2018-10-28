0daacfc7937aa563efe162cee295ef635d70efea
JoshGutman <jng86@nau.edu>
2018-10-27 18:02:59 -0700

    Resolves #9.
    Add "Game Over" screen when player health reaches 0.

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour {
+
+    public PlayerHealth playerHealth;
+    public float restartDelay = 5f;
+
+    Animator anim;
+    float restartTimer;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+            restartTimer += Time.deltaTime;
+
+            if (restartTimer >= restartDelay)
+            {
+                Application.LoadLevel(Application.loadedLevel);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+	}
+}
