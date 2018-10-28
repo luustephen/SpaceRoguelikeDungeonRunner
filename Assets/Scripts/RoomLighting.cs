89e1111731d7033002ccc6b452efdfd9490ff7ce
Computer321 <stephen.luu@sbcglobal.net>
2018-10-27 21:58:35 -0700

    Updated lighting to dynamically light rooms

--- Assets/Scripts/RoomLighting.cs
+++ Assets/Scripts/RoomLighting.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class RoomLighting : MonoBehaviour {
 
     private GameObject[] players;
-    private Light roomLight;
+    private GameObject[] roomLight;
     private Transform leftmostWall, rightmostWall, upmostWall, downmostWall;
+    private GameObject floor;
     private int numPlayers;
+    private int numLights;
+    LightmapData[] lightmap_data;
 
     // Use this for initialization
     void Start() {
         numPlayers = 1;                                 //Change for multiplayer
+        numLights = 4;
+        roomLight = new GameObject[numLights];
+        lightmap_data = LightmapSettings.lightmaps;
         for (int i = 0; i < numPlayers; i++) { 
             players = GameObject.FindGameObjectsWithTag("Player");
         }
-        roomLight = transform.Find("room light").GetComponent<Light>();
+        floor = transform.Find("floor").gameObject;
         leftmostWall = transform.Find("leftmost wall");
         rightmostWall = transform.Find("rightmost wall");
         upmostWall = transform.Find("upmost wall");
@@ -25,17 +31,23 @@ public class RoomLighting : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        for (int i = 0; i < numPlayers; i++)
+        for (int i = 0; i < players.Length; i++)
         {
             if (players[i].transform.position.x > leftmostWall.transform.position.x && players[i].transform.position.x < rightmostWall.transform.position.x && players[i].transform.position.y > downmostWall.transform.position.y && players[i].transform.position.y < upmostWall.transform.position.y)
             {
-                roomLight.enabled = true;
-                //print(leftmostWall.transform.position.x + " " + rightmostWall.transform.position.x + " " + upmostWall.transform.position.x + " " + downmostWall.transform.position.x);
-                //print(players[i].transform.position.x + " " + players[i].transform.position.y);
+                foreach (Transform child in transform) if (child.CompareTag("Light"))
+                    {
+                        child.gameObject.GetComponent<Light>().enabled = true;
+                    }
+                floor.layer = 9;
             }
             else
             {
-                roomLight.enabled = false;
+                foreach (Transform child in transform) if (child.CompareTag("Light"))
+                    {
+                        child.gameObject.GetComponent<Light>().enabled = false;
+                    }
+                floor.layer = 10;
             }
         }
 	}
