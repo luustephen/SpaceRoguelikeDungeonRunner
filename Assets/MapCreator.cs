f37d6ee13315e9235e299e7b8ebe6fec8697da1a
Computer321 <stephen.luu@sbcglobal.net>
2018-11-11 00:13:50 -0800

    builds the rooms a bit more smartly, still needs to place them

--- Assets/MapCreator.cs
+++ Assets/MapCreator.cs
@@ -15,9 +15,13 @@ public class MapCreator : MonoBehaviour
         public int numLeftDoors, numRightDoors, numUpDoors, numDownDoors;
         public int exits;
         public bool isHallway;
+        public Room[] upRooms = new Room[2];
+        public Room[] downRooms = new Room[2];
+        public Room[] leftRooms = new Room[2];
+        public Room[] rightRooms = new Room[2];
     }
 
-    public GameObject[] prebuiltRooms;
+    public GameObject[] prebuiltRooms;              //Array of gameobjects for the editor
     private Room[] rooms;                           //Array that holds information about the gameobjects in prebuiltRooms
     public GameObject startingRoom;
     public GameObject endingRoom;
@@ -77,26 +81,33 @@ public class MapCreator : MonoBehaviour
 
     GameObject[] FindEssentialPath()
     {
-        GameObject[] path = new GameObject[numRooms / 4 * 3];
+        GameObject[] path = new GameObject[numRooms / 4 * 3];       //Array of gameobjects for path objects
+        Room[] pathRooms = new Room[numRooms / 4 * 3];              //Room information for path array
         int currentNumRooms = numRooms - 2;
-        path[0] = startingRoom;
-        for (int k = 1; k < path.Length - 2; k++)
+        path[0] = prebuiltRooms[0];                                 //First room on array is starting room
+        pathRooms[0] = rooms[0];                                    //Enter room information for starting room
+
+        for (int k = 1; k < path.Length - 2; k++)                   //Iterate through path minus starting and ending rooms
         {
             if (currentNumRooms > 0)
             {
                 int randomRoom = (int)Random.Range(0, prebuiltRooms.Length);
                 if (k % 2 == 1)
                 {
-                    while (!rooms[randomRoom].isHallway)
+                    while (!rooms[randomRoom].isHallway || !checkRoomConnect(pathRooms[k-1],rooms[randomRoom]))    //Change room type until compatible hallway
                         randomRoom = (int)Random.Range(0, prebuiltRooms.Length);
                     path[k] = prebuiltRooms[randomRoom];    //Change for map seeding?
+                    pathRooms[k] = rooms[randomRoom];
+                    connectRooms(pathRooms[k-1],pathRooms[k]);
                     currentNumRooms = currentNumRooms - rooms[randomRoom].exits;
                 }
                 else
                 {
-                    while (rooms[randomRoom].isHallway)
+                    while (rooms[randomRoom].isHallway || rooms[randomRoom].exits < 2 || !checkRoomConnect(pathRooms[k-1], rooms[randomRoom]))      //For rooms
                         randomRoom = (int)Random.Range(0, prebuiltRooms.Length);
                     path[k] = prebuiltRooms[randomRoom];    //Change for map seeding?
+                    pathRooms[k] = rooms[randomRoom];
+                    connectRooms(pathRooms[k - 1], pathRooms[k]);
                     currentNumRooms = currentNumRooms - rooms[randomRoom].exits;
                 }
                 essentialPathLength = k;
@@ -107,7 +118,81 @@ public class MapCreator : MonoBehaviour
                 continue;
             }
         }
-        path[essentialPathLength] = endingRoom;
+        path[essentialPathLength] = endingRoom;        //Put the ending room onto the end of the path
         return path;
+    }
+
+    bool checkRoomConnect(Room first, Room second)          //Check if two rooms connect
+    {
+        if(first.numLeftDoors > 0 && second.numRightDoors > 0 || first.numRightDoors > 0 && second.numLeftDoors > 0 || first.numUpDoors > 0 && second.numDownDoors > 0 || first.numDownDoors > 0 && second.numUpDoors > 0)
+            return true;
+        else
+            return false;
+    }
+
+    bool connectRooms(Room first, Room second)         //Connect the rooms together 
+    {
+        if (first.numLeftDoors > 0 && second.numRightDoors > 0)
+        {
+            for(int i = 0; i<first.leftRooms.Length-1; i++)
+            {
+                for (int k = 0; k < first.rightRooms.Length-1; k++)
+                {
+                    if (first.leftRooms[i] == null && second.rightRooms[k] == null)
+                    {
+                        first.leftRooms[i] = second;
+                        second.rightRooms[k] = first;
+                        return true;
+                    }
+                }
+            }
+        }
+        if (first.numRightDoors > 0 && second.numLeftDoors > 0)
+        {
+            for (int i = 0; i < first.rightRooms.Length - 1; i++)
+            {
+                for (int k = 0; k < first.leftRooms.Length - 1; k++)
+                {
+                    if (first.rightRooms[i] == null && second.leftRooms[k] == null)
+                    {
+                        first.rightRooms[i] = second;
+                        second.leftRooms[k] = first;
+                        return true;
+                    }
+                }
+            }
+        }
+        if (first.numUpDoors > 0 && second.numDownDoors > 0)
+        {
+            for (int i = 0; i < first.upRooms.Length - 1; i++)
+            {
+                for (int k = 0; k < first.downRooms.Length - 1; k++)
+                {
+                    if (first.upRooms[i] == null && second.downRooms[k] == null)
+                    {
+                        first.upRooms[i] = second;
+                        second.downRooms[k] = first;
+                        return true;
+                    }
+                }
+            }
+        }
+        if (first.numDownDoors > 0 && second.numUpDoors > 0)
+        {
+            for (int i = 0; i < first.downRooms.Length - 1; i++)
+            {
+                for (int k = 0; k < first.upDoors.Length - 1; k++)
+                {
+                    if (first.downRooms[i] == null && second.upRooms[k] == null)
+                    {
+                        first.downRooms[i] = second;
+                        second.upRooms[k] = first;
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
