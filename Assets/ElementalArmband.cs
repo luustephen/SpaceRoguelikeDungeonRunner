fa1114bef4569cfb2da58de84855360dd76c24ed
Computer321 <stephen.luu@sbcglobal.net>
2019-07-13 22:08:17 -0700

    made armband reflect current element of the main attack

---
+++ Assets/ElementalArmband.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementalArmband : MonoBehaviour {
+
+    private ElementalEffects elementScript;
+    private ElementalEffects attackElementScript;
+
+	// Use this for initialization
+	void Start () {
+        elementScript = gameObject.GetComponent<ElementalEffects>();
+        attackElementScript = transform.parent.GetComponentInChildren<ElementalEffects>(); //Pick up the element of the first child on the Player object, this should be the atk hitbox
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (elementScript && attackElementScript)
+        {
+            elementScript.element = attackElementScript.element;
+        }
+	}
+}
