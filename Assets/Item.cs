a4c25cc2331cdfdaf197c67ea888562149252ca5
Computer321 <stephen.luu@sbcglobal.net>
2019-10-10 21:40:57 -0700

    Moved some scripts around and added some basic inventory tracking

---
+++ Assets/Item.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item : MonoBehaviour {
+
+    public const int PASSIVE = 0;
+    public const int ACTIVE = 1;
+    public const int KNIFE = 0;
+    public const int ARMCANNON = 1;
+
+    public int quantity;
+    public enum Part
+    {
+        KNIFE,
+        ARMCANNON
+    };
+    public enum Type
+    {
+        PASSIVE,
+        ACTIVE
+    };
+    public Type type;
+    public Part part;
+    public string name;
+
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    private List<Item> itemInventory;
+
+	// Use this for initialization
+	void Start () {
+        itemInventory = new List<Item>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach(Item printitem in itemInventory)
+        {
+            print(printitem.name);
+        }
+	}
+
+    public bool Add(Item addedItem)
+    {
+        if (itemInventory.Contains(addedItem))//Increase quantity of item if already held
+        {
+            itemInventory[itemInventory.IndexOf(addedItem)].quantity++;
+        }
+        else //or add it if it doesn't exist
+            itemInventory.Add(addedItem);
+        return true;
+    }
+}
