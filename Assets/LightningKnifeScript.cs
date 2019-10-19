a54b9cdf7ebce13131b83bb435c59182b950bec0
Computer321 <stephen.luu@sbcglobal.net>
2019-10-18 17:37:49 -0700

    Updated project to 2019.2.9f1

---
+++ Assets/LightningKnifeScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningKnifeScript : Item {
+
+    private ElementalEffects elementScript;
+    private GameObject playerWeapon;
+    private GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        foreach (Transform child in player.transform)
+        {
+            if (child.tag == "Player Attack")
+            {
+                playerWeapon = child.gameObject;
+                elementScript = child.GetComponent<ElementalEffects>();
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void UseItem()
+    {
+        if (elementScript != null)
+        {
+            elementScript.element = elementScript.element | LIGHTNING;
+            elementScript.elementChangedBy = gameObject;
+            print("AYAYA");
+        }
+    }
+}
